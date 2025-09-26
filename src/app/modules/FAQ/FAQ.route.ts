72b0bc1c4d5b8e8aa4605152fa84c94a50f53c1a
sayedhasan019283 <sayedhasan019283@gmail.com>
2025-09-27 04:06:15 +0600

    add faq model Route file

--- src/app/modules/FAQ/FAQ.route.ts
+++ src/app/modules/FAQ/FAQ.route.ts
@@ -1,8 +1,37 @@
+import { USER_ROLE } from './../user/user.constant';
 import express from 'express'
+import auth from '../../middlewares/auth'
+import { faqController } from './FAQ.controller'
 
 const router = express.Router()
 // post , get , get single , update, delete
+router.post(
+    '/create',
+    auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+    faqController.createFaq
+)
+router.patch(
+    '/update/:faqId',
+    auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+    faqController.updateFaq
+)
+router.get(
+    '/read-all',
+    auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.analyst, USER_ROLE.user),
+    faqController.readAllFaq
+)
 
+router.get(
+    '/get-single-faq/:faqId',
+    auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+    faqController.readSingleFaq
+)
+
+router.delete(
+    '/delete/:faqId',
+    auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+    faqController.deleteFaq
+)
 
 
 export const faqRouter = router
